94cad62175c317a398101eb18622db20087ee407
Pauline Kaoomi <pauav22015@gmail.com>
2025-04-15 17:49:31 -0500

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,10 +16,10 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Index',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Index', slug: 'scr/pages/index.mdx' },
 					],
 				},
 				{
--- src/components/clickExample.tsx
+++ src/components/clickExample.tsx
@@ -1,18 +1,18 @@
 
 import { useState } from 'react'
 
-export default function clickExample() {
-  const [clicks, setClicks] = useState(0)
+export default function ClickExample() {
+  const [clicks, setClicks] = useState(0);
 
   return (
     <div style={{ padding: '1rem', border: '1px solid #ccc', borderRadius: '8px' }}>
-      <p>Has hecho clic {clicks} veces</p>
+      
       <button
         onClick={() => setClicks(clicks + 1)}
-        style={{ padding: '0.5rem 1rem', backgroundColor: '#0070f3', color: '#fff', border: 'none', borderRadius: '4px' }}
-      >
+        style={{ padding: '0.5rem 1rem', backgroundColor: '#0070f3', color: '#fff', border: 'none', borderRadius: '4px' }}>
         ¡Haz clic!
       </button>
+      <p>Has hecho clic {clicks} veces</p>
     </div>
   )
 }
